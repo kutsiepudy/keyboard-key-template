eef8fdbd73faa4f0a9b1a77f333e355047a3e986
Kristin <kutsiepudy3841@gmail.com>
2026-08-27 10:06:34 -0700

    Integrate keyboard functionality for button press

--- code.py
+++ code.py
@@ -1,15 +1,23 @@
 import board
 import digitalio
 from adafruit_debouncer import Button
+from adafruit_debouncer import Keyboard
+from adafruit_debouncer import Keycode
+
+keyboard = Keyboard(usb_hid.device)
+
+
 
 key_pin = digitalio.DigitalInOut(board.GP9)
 key_pin.direction = digitalio.Direction.INPUT
 key_pin.pull = digitalio.Pull.UP
-
 key = Button(key_pin, value_when_pressed=False)
 
 while True:
     key.update()
 
     if key.pressed:
-        print("q")
+        keyboard.press(Keycode.Q)
+
+    if key.released:
+        keyboard.release(Keycode.Q)
